6d0f8299beca31ea784d40d027f4ece3da351cab
rc-li <husky.li.ruicheng@gmail.com>
2020-06-04 15:28:54 -0700

    style the cards and set visibility roughly

--- hw6 updated/backend/static/script.js
+++ hw6 updated/backend/static/script.js
@@ -8,9 +8,10 @@ if (ele.addEventListener) {
 
 var result;
 var num_entries;
+var params;
 function callFetch() {
     const formData = new FormData(form);
-    const params = new URLSearchParams(formData);
+    params = new URLSearchParams(formData);
     console.log(params.toString());
 
     req = new XMLHttpRequest();
@@ -22,25 +23,29 @@ function callFetch() {
     num_entries = obj.findItemsAdvancedResponse[0].paginationOutput[0].totalEntries[0]
     searchResult = obj.findItemsAdvancedResponse[0].searchResult[0];
     updateSummary();
-    // setData(0)
     setAllData();
 }
 
 function setData(i) {
+    // set card image
     let doc = document.getElementById(i);
     doc.getElementsByTagName("img")[0].src = searchResult.item[i].galleryURL
 
+    // set the product link
     let div = doc.getElementsByTagName('div')[0]
     let a = div.getElementsByTagName('a')[0]
     a.innerHTML = searchResult.item[i].title
     a.href = searchResult.item[i].viewItemURL[0]
 
+    // set the category
     let catagory = div.getElementsByClassName('category')[0]
     catagory.innerHTML = 'Category: ' + searchResult.item[i].primaryCategory[0].categoryName[0]
 
+    // set the redirect image link
     let redirectIMGLink = div.getElementsByClassName('redirectIMGLink')[0]
     redirectIMGLink.href = searchResult.item[i].viewItemURL[0]
 
+    // set the product condition and top rated image visibility
     let condition = div.getElementsByClassName('condition')[0]
     condition.innerHTML = 'Condition: ' + searchResult.item[i].condition[0].conditionDisplayName[0]
     let topRatedIMG = div.getElementsByClassName('topRatedIMG')[0]
@@ -52,6 +57,7 @@ function setData(i) {
         topRatedIMG.style.visibility = 'hidden'
     }
 
+    // set if return is accepted
     let accptReturn = div.getElementsByClassName('accptReturn')[0]
     let isAccepted = searchResult.item[i].returnsAccepted[0]
     if (isAccepted) {
@@ -61,6 +67,7 @@ function setData(i) {
         accptReturn.innerHTML = 'Seller does not accept returns'
     }
 
+    // set shipping type
     let shipType = div.getElementsByClassName('shipType')[0]
     let shippingServiceCost = searchResult.item[0].shippingInfo[0].shippingServiceCost[0].__value__
     if (shippingServiceCost == 0.0) {
@@ -70,6 +77,7 @@ function setData(i) {
         shipType.innerHTML = 'No Free Shipping'
     }
 
+    // set price
     let priceTag = div.getElementsByClassName('price')[0]
     let priceValue = parseInt(searchResult.item[0].sellingStatus[0].convertedCurrentPrice[0].__value__)
     let shipFrom = searchResult.item[i].location[0]
@@ -80,17 +88,23 @@ function setData(i) {
         priceTag.innerHTML = 'Price: $' + priceValue
     }
 
+    // set ship from
     let locTag = div.getElementsByClassName('priceTag_loc')[0]
     locTag.innerHTML = '<i>From ' + shipFrom + '</i>'
 
 }
 
 function setAllData() {
+    let summary = document.getElementById('summary')
+    summary.style.display = 'block'
+    let card = document.getElementById('cards')
+    card.style.display = 'block'
     for (var i = 0; i < 3; i++) {
         setData(i);
     }
 }
 
 function updateSummary() {
-    document.getElementById('summary').innerHTML = 'Found ' + num_entries + ' entries'
+    let keyword = params.toString().split('&')[0].split('=')[1]
+    document.getElementById('summary').innerHTML = num_entries + ' Results found for <i>' + keyword + '</i><hr>'
 }
