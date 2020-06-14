701ef1bada497c8f3ddf4a76a3c4c14c5f219ae7
rc-li <husky.li.ruicheng@gmail.com>
2020-06-14 00:29:31 -0700

    list component is up

--- homework8/newProj/src/app/app.module.ts
+++ homework8/newProj/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { CardsComponent } from './cards/cards.component';
@@ -22,6 +23,7 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
+    NgxPaginationModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule
