29f076d1c707b14ae1ba856532e27c209a5d018d
rc-li <husky.li.ruicheng@gmail.com>
2020-06-14 13:48:03 -0700

    static file hosted from disk on express server

--- homework8/nodeServer/index.js
+++ homework8/nodeServer/index.js
@@ -4,7 +4,13 @@ const debug = require('debug')('myapp:server');
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static('../newProj/dist/newProj'))
+
 app.listen(port, () => {
     //  console.log('Server is up and running on port ', port);
     debug('Server is up and running on port ', port);
+})
+
+app.get('/', function (req, res) {
+    return res.send()
 })
