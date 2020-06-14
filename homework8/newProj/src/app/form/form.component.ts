8690d6fd29e870ee9afd5fba73dc617e49b27f58
rc-li <husky.li.ruicheng@gmail.com>
2020-06-14 15:21:10 -0700

    form check migrated to Typescript
    it's more trouble dealing with typescript syntax

--- homework8/newProj/src/app/form/form.component.ts
+++ homework8/newProj/src/app/form/form.component.ts
@@ -12,4 +12,22 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  valueCheck() {
+    alert('value check called')
+    let lowPriceStr = (<HTMLInputElement>document.getElementById('lowPrice')).value
+    let highPriceStr = (<HTMLInputElement>document.getElementById('highPrice')).value
+    let lowPrice = parseFloat(lowPriceStr)
+    let highPrice = parseFloat(highPriceStr)
+    if (lowPrice < 0.0) {
+        alert("Price Range values cannot be negative! Please try a value greater than or equal to 0.0")
+        return false
+    }
+    else if (lowPrice > highPrice) {
+        alert("Oops! Lower price limit cannot be greater than upper price limit! Please try again.")
+        return false
+    }
+    else return true
+  }
 }
+
+
