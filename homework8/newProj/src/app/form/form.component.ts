b0231a175647ad65605ec7e5a747c2e07f947924
rc-li <husky.li.ruicheng@gmail.com>
2020-06-17 18:53:42 -0700

    form validation implemented

--- homework8/newProj/src/app/form/form.component.ts
+++ homework8/newProj/src/app/form/form.component.ts
@@ -13,20 +13,52 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  emptyKeywordAlert: boolean = false;
+  minmaxAlert: boolean = false;
+  responseEmptyAlert: boolean = false;
+
   valueCheck():boolean {
+    let retVal: boolean = true;
+
+    //check for empty keyword
+    let keyWord = (<HTMLInputElement>document.getElementsByTagName('input')[0]).value
+    if (keyWord == '') {
+      this.emptyKeywordAlert = true;
+      retVal = false;
+    }
+    else {
+      this.emptyKeywordAlert = false;
+    }
+
+    //check for minPrice and maxPrice
     let lowPriceStr = (<HTMLInputElement>document.getElementById('lowPrice')).value
     let highPriceStr = (<HTMLInputElement>document.getElementById('highPrice')).value
     let lowPrice = parseFloat(lowPriceStr)
     let highPrice = parseFloat(highPriceStr)
     if (lowPrice < 0.0) {
-      alert("Price Range values cannot be negative! Please try a value greater than or equal to 0.0")
-      return false
+      this.minmaxAlert = true
+      retVal = false
     }
     else if (lowPrice > highPrice) {
-      alert("Oops! Lower price limit cannot be greater than upper price limit! Please try again.")
+      this.minmaxAlert = true
+      retVal = false
+    }
+    else if (highPrice < 0.0) {
+      this.minmaxAlert = true
+      retVal = false
+    }
+    else {
+      this.minmaxAlert = false
+    }
+
+    //if any of the check fails, return false
+    if (retVal == true) {
+      return true
+    }
+    else {
       return false
     }
-    else return true
+
   }
   
   keyword: string;
@@ -34,9 +66,12 @@ export class FormComponent implements OnInit {
   data: any;
   dataLength: number;
 
-  callFetch():void {
+  searchClicked():void {
+    this.data = null
+    this.dataLength = 0
     if (this.valueCheck()) {
       // str:String = String.toString(formData)
+
       let form = document.getElementsByTagName('form')[0]
       let formData = new FormData(form)
       
@@ -54,6 +89,13 @@ export class FormComponent implements OnInit {
           this.dataLength = this.data.length
           this.keyword = document.getElementsByTagName('input')[0].value
           console.log(this.keyword)
+
+          if (this.data.length == 0) {
+            this.responseEmptyAlert = true
+          }
+          else {
+            this.responseEmptyAlert = false
+          }
         })
 
       
