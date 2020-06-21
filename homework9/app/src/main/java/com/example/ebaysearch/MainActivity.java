6301edada3774efebafeaa45207e4c9917e17885
rc-li <husky.li.ruicheng@gmail.com>
2020-06-20 22:57:10 -0700

    clear button functionality implemented

--- homework9/app/src/main/java/com/example/ebaysearch/MainActivity.java
+++ homework9/app/src/main/java/com/example/ebaysearch/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -43,5 +45,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     public void searchClicked(View view) {
 
+    }
+
+    public void clearClicked(View view) {
+        EditText keyword = findViewById(R.id.keywordEdit);
+        keyword.setText("");
+        EditText lowPrice = findViewById(R.id.lowPrice);
+        lowPrice.setText("");
+        EditText highPrice = findViewById(R.id.highPrice);
+        highPrice.setText("");
+
+        CheckBox isNew = findViewById(R.id.isNew);
+        isNew.setChecked(false);
+        CheckBox isUsed = findViewById(R.id.isUsed);
+        isUsed.setChecked(false);
+        CheckBox isUnspecified = findViewById(R.id.isUnspecified);
+        isUnspecified.setChecked(false);
+
+        Spinner spinner = findViewById(R.id.optionBar);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
