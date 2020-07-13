980e900218a090a255eac6f152bda69a57f34e83
rc-li <husky.li.ruicheng@gmail.com>
2020-07-13 03:11:34 -0700

    changed the style of web app a little

--- homework8/nodeServer/static/polyfills.js
+++ homework8/nodeServer/static/polyfills.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Ruicheng\Documents\GitHub\571-homeworks\homework8\newProj\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\github\571-homeworks\homework8\newProj\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
